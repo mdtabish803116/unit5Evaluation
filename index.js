148cf6dae4c9b0fa9f16dd58a3bf756b66f8390a
mdtabish803116 <mdtabish803116@gmail.com>
2022-04-26 14:07:23 +0530

    added modified files

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ pokemonArr.forEach(function(pokemon){
 })
 
 
-var pokemonVal = document.getElementById("searchBox").value;
 
 document.getElementById("searchBox").addEventListener("change" , function(){ 
-var myResponse = fetch(`https://pokeapi.co/api/v2/pokemon/raticate`);
+    var pokemonVal = document.getElementById("searchBox");
+var myResponse = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonVal.value}`);
 
 myResponse.then(function(res){
     res.json().then(function(result){
@@ -55,8 +55,29 @@ function displayData(data){
           heightDiv.textContent = `Height : ${data.height}`;
           var weightDiv = document.createElement("div");
              weightDiv.textContent = `Weight : ${data.weight}`;
+          var abilitiesDiv = document.createElement("div");
+               abilitiesDiv.setAttribute("id" , "abilitiesDiv");
+                var textAbility = document.createElement("div");
+                   textAbility.innerText = "Abilities : "
+                   abilitiesDiv.append(textAbility);
+            data.abilities.forEach(function(each){
+                 var abilityDiv = document.createElement("div")
+                     abilityDiv.innerText = `${each.ability.name} ,`;
+                abilitiesDiv.append(abilityDiv);
+            })
+
+            var movesDiv = document.createElement("div");
+            movesDiv.setAttribute("id" , "movesDiv");
+             var textMove = document.createElement("div");
+                textMove.innerText = "Moves : "
+                movesDiv.append(textMove);
+         data.moves.forEach(function(each){
+              var moveDiv = document.createElement("div")
+                  moveDiv.innerText = `${each.move.name} ,`;
+             movesDiv.append(moveDiv);
+         })
       
-      pokeCart.append(idDiv , nameDiv , heightDiv , weightDiv);
+      pokeCart.append(idDiv , nameDiv , heightDiv , weightDiv , abilitiesDiv , movesDiv);
       
       document.getElementById("pokeContainer").append(pokeCart);
 
@@ -65,3 +86,60 @@ function displayData(data){
 
 })
 
+
+document.getElementById("searchIcon").addEventListener("click" , function(){ 
+    var pokemonVal = document.getElementById("searchBox");
+var myResponse = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonVal.value}`);
+
+myResponse.then(function(res){
+    res.json().then(function(result){
+
+         var searchData = result;
+         displayData(searchData);
+             console.log(searchData);
+    })
+}).catch(function(err){
+    console.log(err);
+})
+
+function displayData(data){
+    document.getElementById("pokeContainer").innerText = "";
+     var pokeCart = document.createElement("div");
+       var idDiv = document.createElement("div");
+         idDiv.textContent = `id : ${data.id}`;
+        var nameDiv = document.createElement("div");
+          nameDiv.textContent = data.name;
+         var heightDiv = document.createElement("div");
+          heightDiv.textContent = `Height : ${data.height}`;
+          var weightDiv = document.createElement("div");
+             weightDiv.textContent = `Weight : ${data.weight}`;
+          var abilitiesDiv = document.createElement("div");
+               abilitiesDiv.setAttribute("id" , "abilitiesDiv");
+                var textAbility = document.createElement("div");
+                   textAbility.innerText = "Abilities : "
+                   abilitiesDiv.append(textAbility);
+            data.abilities.forEach(function(each){
+                 var abilityDiv = document.createElement("div")
+                     abilityDiv.innerText = `${each.ability.name} ,`;
+                abilitiesDiv.append(abilityDiv);
+            })
+
+            var movesDiv = document.createElement("div");
+            movesDiv.setAttribute("id" , "movesDiv");
+             var textMove = document.createElement("div");
+                textMove.innerText = "Moves : "
+                movesDiv.append(textMove);
+         data.moves.forEach(function(each){
+              var moveDiv = document.createElement("div")
+                  moveDiv.innerText = `${each.move.name} ,`;
+             movesDiv.append(moveDiv);
+         })
+      
+      pokeCart.append(idDiv , nameDiv , heightDiv , weightDiv , abilitiesDiv , movesDiv);
+      
+      document.getElementById("pokeContainer").append(pokeCart);
+
+}
+
+
+})
